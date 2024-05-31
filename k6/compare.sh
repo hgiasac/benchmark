709eac95daef163adc8a54d763f0dd92f550718a
Toan Nguyen <hgiasac@gmail.com>
2024-06-01 01:06:47 +0700

    improve comparison plots

--- k6/compare.sh
+++ k6/compare.sh
@@ -4,6 +4,8 @@ FILES="$@"
 
 LATENCY_OUTPUT=output/latency-comparison
 RPS_OUTPUT=output/rps-comparison.png
+CPU_OUTPUT=output/cpu-comparison.png
+RAM_OUTPUT=output/ram-comparison.png
 
 # export latency comparison
 rm -f $LATENCY_OUTPUT.csv $LATENCY_OUTPUT.tmp
@@ -41,15 +43,66 @@ done
     echo "#plot commands"
     echo "set terminal pngcairo background rgb 'white' linewidth 4 size 1920,1080 enhanced font 'Arial,16'"
     echo "set output '$RPS_OUTPUT'"
-		echo "set title 'Latency comparison' font 'Arial Bold,20'"
+		echo "set title 'RPS comparison' font 'Arial Bold,20'"
 		echo "set style data lines"
 		echo "set style line 100 lt 1 lc rgb 'grey' lw 0.5"
 		echo "set grid ls 100"
-		echo "set ylabel 'Latency (milliseconds)'"
+		echo "set ylabel 'RPS'"
     echo "set key top left"
     echo "set xlabel 'Time (M:S)'"
     echo "set xdata time"
     echo "set format x '%M:%S'"
 
     echo "plot $plotcmds"
+) | gnuplot
+
+# export CPU comparison
+for file in $FILES; do
+  if [ -n "$cpu_plotcmds" ]; then
+    cpu_plotcmds="$cpu_plotcmds,"
+  fi
+	cpu_plotcmds="$cpu_plotcmds '$file' using (\$1):2 with lines title '$(basename $file)'"
+done
+
+(
+    echo "#plot commands"
+    echo "set terminal pngcairo background rgb 'white' linewidth 4 size 1920,1080 enhanced font 'Arial,16'"
+    echo "set output '$CPU_OUTPUT'"
+		echo "set title 'CPU comparison' font 'Arial Bold,20'"
+		echo "set style data lines"
+		echo "set style line 100 lt 1 lc rgb 'grey' lw 0.5"
+		echo "set grid ls 100"
+		echo "set ylabel 'Percent (%)'"
+    echo "set key top left"
+    echo "set xlabel 'Time (M:S)'"
+    echo "set xdata time"
+    echo "set format x '%M:%S'"
+
+    echo "plot $cpu_plotcmds"
+) | gnuplot
+
+# export memory comparison
+for file in $FILES; do
+  if [ -n "$ram_plotcmds" ]; then
+    ram_plotcmds="$ram_plotcmds,"
+  fi
+	ram_plotcmds="$ram_plotcmds '$file' using (\$1):(getMemoryBytes(stringcolumn(3))) with lines title '$(basename $file)'"
+done
+
+(
+    echo "#plot commands"
+    echo "set terminal pngcairo background rgb 'white' linewidth 4 size 1920,1080 enhanced font 'Arial,16'"
+    echo "set output '$RAM_OUTPUT'"
+		echo "set title 'Memory comparison' font 'Arial Bold,20'"
+		echo "set style data lines"
+		echo "set style line 100 lt 1 lc rgb 'grey' lw 0.5"
+		echo "set grid ls 100"
+    echo "set ylabel 'Memory usage'"
+    echo "set format y '%.1s%cB'"
+    echo "set key top left"
+    echo "set xlabel 'Time (M:S)'"
+    echo "set xdata time"
+    echo "set format x '%M:%S'"
+    echo "getMemoryBytes(x)=(real(x*1024))"
+    echo "plot $ram_plotcmds"
 ) | gnuplot
--- k6/latency-bars.gnuplot
+++ k6/latency-bars.gnuplot
@@ -5,6 +5,8 @@ infile=ARG1
 outfile=ARG2
 ptitle=ARG3
 
+stats infile u 0 nooutput
+
 set terminal pngcairo background rgb 'white' linewidth 4 size 1920,1080 enhanced font 'Arial,16'
 set output outfile
 
@@ -17,6 +19,7 @@ set tics out
 set ylabel 'Latency (ms)'
 set ytics
 set autoscale y
+set yrange [0:*]
 
 set style histogram clustered
 set style fill solid
@@ -24,6 +27,12 @@ set boxwidth 0.9
 
 set style line 1 lc rgb "#C9190B"
 set style line 2 lc rgb "#0066CC"
+set style line 3 lc rgb "#009e73"
+set style line 4 lc rgb "dark-violet"
+set style line 5 lc rgb "gray50"
+set style line 6 lc rgb "#0072b2"
+set style line 7 lc rgb "dark-green"
+set style line 8 lc rgb "#56b4e9"
+set style line 9 lc rgb "#e69f00"
 
-plot infile using 2:xtic(1) with histograms ls 1 title columnhead, \
-  infile using 3:xtic(1) with histograms ls 2 title columnhead
+plot for [COL=2:STATS_columns] infile using COL:xtic(1) with histograms ls (COL-1) title columnhead
